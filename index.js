f2c0300061541b5254eac96d700169fc3e482113
Georg Ekeberg <georghagen@gmail.com>
2021-01-08 16:36:35 +0100

    added things

---
+++ index.js
@@ -0,0 +1,8 @@
+function TestsFunction() {
+    const T = document.querySelector(".dogs");
+    T.style.display = "block";  // <-- Set it to block
+}
+// function KaviarFunction() {
+//     const K = document.querySelector(".kaviar");
+//     K.style.display = "block";  // <-- Set it to block
+// }
